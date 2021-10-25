76eed84b3d002f271e5d1b1463595bbac2802cb7
gabriellenon <gabriellennon7@gmail.com>
2021-10-25 20:33:59 -0300

    aula 3

--- perfis/views.py
+++ perfis/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from perfis.models import Perfil
 
-# Create your views here.
+def index(request):
+	return render(request, 'index.html')
+
+def exibir(request, perfil_id):
+	
+	perfil = Perfil()
+
+	if perfil_id == 1:
+		perfil = Perfil('Fábio Henrique', 'fabio.oliveira@ifb.edu.br', '222222', 'IFB')
+
+
+	return render(request, 'perfil.html', {'perfil':perfil})
