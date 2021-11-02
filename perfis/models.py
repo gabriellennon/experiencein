9660555b41dc3979ba284ffa886414706bc8982c
gabriellenon <gabriellennon7@gmail.com>
2021-11-02 13:07:48 -0300

    module 4

--- perfis/models.py
+++ perfis/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-class Perfil(object):
+class Perfil(models.Model):
 
-	def __init__(self, nome='', email='', telefone='', nome_empresa=''):
-		self.nome = nome
-		self.email = email
-		self.telefone = telefone
-		self.nome_empresa = nome_empresa		
+	# ja criando um campo no banco de dados
+    nome = models.CharField(max_length=255, null=False)
+    email = models.CharField(max_length=255, null=False)     
+    telefone = models.CharField(max_length=15, null=False)
+    nome_empresa = models.CharField(max_length=255, null=False)
--- perfis/views.py
+++ perfis/views.py
@@ -5,11 +5,6 @@ def index(request):
 	return render(request, 'index.html')
 
 def exibir(request, perfil_id):
-	
-	perfil = Perfil()
 
-	if perfil_id == 1:
-		perfil = Perfil('Fábio Henrique', 'fabio.oliveira@ifb.edu.br', '222222', 'IFB')
-
-
-	return render(request, 'perfil.html', {'perfil':perfil})
+    perfil = Perfil.objects.get(id=perfil_id)
+    return render(request, 'perfil.html', { "perfil" : perfil})
