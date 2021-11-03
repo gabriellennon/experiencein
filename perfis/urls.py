6ad423c803d2258e73a7201ee1aeb5f2af85b88a
gabriellenon <gabriellennon7@gmail.com>
2021-11-03 19:49:47 -0300

    aula 6

--- perfis/urls.py
+++ perfis/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
-	path('perfis/<int:perfil_id>', views.exibir, name='exibir')
+	path('perfis/<int:perfil_id>', views.exibir, name='exibir'),
+	path('perfis/<int:perfil_id>/convidar', views.convidar, name='convidar')
 	# re_path(r'^perfis/\d+$', views.exibir, name='perfis')
 ]
